77be44ceae0ba8d5a10a6f7a1e7f2108aeaf168b
APereira <A07pereira@gmail.com>
2021-06-16 14:50:48 -0400

    final push

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 const { notes } = require('../../db/db');
 const { createNewNote, findById, editNote, removeNote } = require('../../lib/notes');
 
